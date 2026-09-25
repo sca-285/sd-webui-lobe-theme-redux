3f763eb22797d33d34028e24ad7682daa6112008
Sca285 <vuhoang.ai97@gmail.com>
2026-09-25 17:36:40 +0700

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -4,7 +4,7 @@
   window.__LOBE_THEME_ENTRY__ = true;
   var script = document.currentScript;
   var base = (script && script.src) || location.href;
-  import(new URL("chunks/main-B5BYn-me.js", base).href).catch(function (error) {
+  import(new URL("chunks/main-BpYneA3y.js", base).href).catch(function (error) {
     console.error('[Lobe Theme Redux] could not load', error);
   });
 })();
